35f8f7da6238186d6bfbd7be33a79e1f8ff2a75b
samir791 <samiryusifov226@gmail.com>
2022-12-29 02:17:38 +0400

    sas

--- js/main.js
+++ js/main.js
@@ -16,10 +16,10 @@ function left() {
 
     if (ss === 0) {
         ss = length
-     
+
         console.log("left1", ss)
-        if(ss===length){
-            ss-=1
+        if (ss === length) {
+            ss -= 1
             console.log("left44", ss)
         }
     }
@@ -37,18 +37,18 @@ function left() {
         slide.style.transform = `translateX(${(index - ss) * 100}%)`
     })
 
-    rightBtn.addEventListener("click",()=>{
-       
+    rightBtn.addEventListener("click", () => {
+
     })
 }
 console.log(left())
 leftBtn.addEventListener("click", left)
 
 // setInterval(left,3000)
- ss = 0
+ss = 0
 // let sildew2 = -1
 function rightbutton() {
-    
+
     if (ss === length - 1) {
         ss = 0
         // sildew++
@@ -59,8 +59,8 @@ function rightbutton() {
         console.log("right2", ss)
     }
     console.log(ss)
-    
-    
+
+
     slides.forEach((slide, index) => {
         // console.log(slide, index)
         slide.style.transform = `translateX(${(index - ss) * 100}%)`
@@ -80,14 +80,14 @@ let length3 = slick2.length
 
 function slickfunction() {
 
-  
+
 
     if (saygac === 0) {
         saygac = length2
         console.log("left1", saygac)
 
-        if(saygac===length2){
-        saygac-=3
+        if (saygac === length2) {
+            saygac -= 3
         }
     }
     else {
@@ -103,8 +103,8 @@ function slickfunction() {
         // console.log(index2)
 
     })
-    rightBtn2.addEventListener("click",()=>{
-       
+    rightBtn2.addEventListener("click", () => {
+
     })
 
 }
@@ -112,7 +112,7 @@ console.log(slickfunction())
 leftBtn2.addEventListener("click", slickfunction)
 // console.log(slickfunction()) 
 
-saygac=0
+saygac = 0
 function slickfunction2() {
 
     if (saygac === length2 - 3) {
@@ -166,16 +166,12 @@ rightBtn2.addEventListener("click", slickfunction2)
 const single3 = document.querySelectorAll(".slick3")
 const rightBtn3 = document.querySelector(".right3")
 const leftBtn3 = document.querySelector(".left3")
-let container = 0
+let container = 1
 let length4 = single3.length
 
 function slick3() {
 
-    single3.forEach((slide4, index4) => {
 
-        slide4.style.transform = `translateX(${(index4 - container) * 100}%)`
-
-    })
 
     if (container === 0) {
         container = length4 - 3
@@ -185,26 +181,35 @@ function slick3() {
         container--
 
     }
+    single3.forEach((slide4, index4) => {
+
+        slide4.style.transform = `translateX(${(index4 - container) * 100}%)`
+
+    })
+
+    rightBtn3.addEventListener("click", () => {
+
+    })
 }
 slick3()
 leftBtn3.addEventListener("click", slick3)
 
-let say = 0
+container = 0
 
 function slickright() {
 
-    if (say === length4 - 3) {
-        say = 0
+    if (container === length4 - 3) {
+        container = 0
         // console.log("sass", say)
     }
     else {
-        say++
+        container++
         // console.log("sss", say)
     }
 
-    single3.forEach((slide5 , index5) => {
+    single3.forEach((slide4, index4) => {
 
-        slide5.style.transform = `translateX(${(index5 - say) * 100}%)`
+        slide4.style.transform = `translateX(${(index4 - container) * 100}%)`
     })
 
 
@@ -212,4 +217,213 @@ function slickright() {
 
 rightBtn3.addEventListener("click", slickright)
 
-setInterval(slickright,3000)
+function set() {
+
+    setInterval(slickright, 3000)
+}
+// set()
+leftBtn3.addEventListener("click", set())
+
+
+let single4 = document.querySelectorAll(".slick4")
+const rightBtn4 = document.querySelector(".right4")
+const leftBtn4 = document.querySelector(".left4")
+let div = document.querySelectorAll(".single_4-container4")
+let list = 1
+let length5 = single4.length
+
+
+function slick4() {
+    if (list === 0) {
+        list = length5 - 1
+
+    }
+    else {
+        list--
+
+    }
+    single4.forEach((slide5, index5) => {
+
+        slide5.style.transform = `translateX(${(index5 - list) * 110}%)`
+
+    })
+    
+    rightBtn4.addEventListener("click", () => {
+
+    })
+
+    if (list === 0) {
+        single4.forEach(list => {
+            list.classList.remove("slic")
+            if (list.textContent.includes("1")) {
+                list.classList.add("slic")
+            }
+        })
+        // console.log("sssss", list)
+    } else if (list === 1) {
+        single4.forEach(list => {
+            list.classList.remove("slic")
+            if (list.textContent.includes("2")) {
+                list.classList.add("slic")
+            }
+        })
+    }
+    else if (list === 2) {
+        single4.forEach(list => {
+            list.classList.remove("slic")
+            if (list.textContent.includes("3")) {
+                list.classList.add("slic")
+            }
+        })
+    }
+    else if (list === 3) {
+        single4.forEach(list => {
+            list.classList.remove("slic")
+            if (list.textContent.includes("4")) {
+                list.classList.add("slic")
+            }
+        })
+    } else if (list === 4) {
+        single4.forEach(list => {
+            list.classList.remove("slic")
+            if (list.textContent.includes("5")) {
+                list.classList.add("slic")
+            }
+        })
+    }
+    else if (list === 5) {
+        single4.forEach(list => {
+            list.classList.remove("slic")
+            if (list.textContent.includes("6")) {
+                list.classList.add("slic")
+            }
+        })
+    }
+    else if (list === 6) {
+        single4.forEach(list => {
+            list.classList.remove("slic")
+            if (list.textContent.includes("7")) {
+                list.classList.add("slic")
+            }
+        })
+    }
+    else if (list === 7) {
+        single4.forEach(list => {
+            list.classList.remove("slic")
+            if (list.textContent.includes("8")) {
+                list.classList.add("slic")
+            }
+        })
+    }
+    else if (list === 8) {
+        single4.forEach(list => {
+            list.classList.remove("slic")
+            if (list.textContent.includes("9")) {
+                list.classList.add("slic")
+            }
+        })
+    }
+}
+slick4()
+leftBtn4.addEventListener("click", slick4)
+
+
+
+
+function slick5() {
+
+    if (list === length5 - 1) {
+        list = 0
+        // console.log("sass", say)
+    }
+    else {
+        list++
+        // console.log("sss", say)
+    }
+    single4.length = 6
+    single4.forEach((slide5, index5) => {
+
+        slide5.style.transform = `translateX(${(index5 - list) * 110}%)`
+    })
+    console.log(single4.length)
+
+    if (list === 0) {
+        single4.forEach(list => {
+            list.classList.remove("slic")
+            if (list.textContent.includes("1")) {
+                list.classList.add("slic")
+            }
+        })
+        // console.log("sssss", list)
+    } else if (list === 1) {
+        single4.forEach(list => {
+            list.classList.remove("slic")
+            if (list.textContent.includes("2")) {
+                list.classList.add("slic")
+            }
+        })
+    }
+    else if (list === 2) {
+        single4.forEach(list => {
+            list.classList.remove("slic")
+            if (list.textContent.includes("3")) {
+                list.classList.add("slic")
+            }
+        })
+    }
+    else if (list === 3) {
+        single4.forEach(list => {
+            list.classList.remove("slic")
+            if (list.textContent.includes("4")) {
+                list.classList.add("slic")
+            }
+        })
+    } else if (list === 4) {
+        single4.forEach(list => {
+            list.classList.remove("slic")
+            if (list.textContent.includes("5")) {
+                list.classList.add("slic")
+            }
+        })
+    }
+    else if (list === 5) {
+        single4.forEach(list => {
+            list.classList.remove("slic")
+            if (list.textContent.includes("6")) {
+                list.classList.add("slic")
+            }
+        })
+    }
+    else if (list === 6) {
+        single4.forEach(list => {
+            list.classList.remove("slic")
+            if (list.textContent.includes("7")) {
+                list.classList.add("slic")
+            }
+        })
+    }
+    else if (list === 7) {
+        single4.forEach(list => {
+            list.classList.remove("slic")
+            if (list.textContent.includes("8")) {
+                list.classList.add("slic")
+            }
+        })
+    }
+    else if (list === 8) {
+        single4.forEach(list => {
+            list.classList.remove("slic")
+            if (list.textContent.includes("9")) {
+                list.classList.add("slic")
+            }
+        })
+    }
+
+    console.log("ssaas",list)
+    
+
+}
+
+
+rightBtn4.addEventListener("click", slick5)
+
